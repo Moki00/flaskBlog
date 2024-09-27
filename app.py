baf4ab73d9d9ed272816c2225d33ee7022ef04de
MoKi <Mokiwins@gmail.com>
2024-09-27 18:21:32 -0400

    can create new posts now

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # Import necessary modules
 import sqlite3
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 
 # Create a Flask application instance
 app = Flask(__name__)
+app.config['SECRET_KEY']='your secret key'
 
-# Define a view function for the main route '/'
+# Main route '/'
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -13,12 +15,29 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+# Single post route
 @app.route('/<int:post_id>')
 def post(post_id):
     #user clicks and saves value in 'post' var
     post = get_post(post_id)
     # render the post page in the html
     return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    # if user clicks Submit
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
 
 # Database Connection
 def get_db_connection():
