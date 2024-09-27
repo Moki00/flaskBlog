f23d52e03c35668997b17dda3fb487255e78b9bb
MoKi <Mokiwins@gmail.com>
2024-09-27 17:01:29 -0400

    flask to html

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+# Import necessary modules
+from flask import Flask, render_template
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define a view function for the main route '/'
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
