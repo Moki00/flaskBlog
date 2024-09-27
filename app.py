cb4534fe73e9c4f5ed4c8d8f4619978638632c94
MoKi <Mokiwins@gmail.com>
2024-09-27 17:31:19 -0400

    loop thru posts

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import necessary modules
+import sqlite3
 from flask import Flask, render_template
 
 # Create a Flask application instance
@@ -7,4 +8,13 @@ app = Flask(__name__)
 # Define a view function for the main route '/'
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+# Database Connection
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
